7cb467de39c3979fa207ce39f62f98196eed7656
Marco Bertini <marco.bertini@unifi.it>
2020-04-20 19:59:44 +0200

    added script to test images dirs

---
+++ test_dirs_imgs.sh
@@ -0,0 +1,7 @@
+python compute_face_dists_images.py -A -v -o results_img -d0 /data/acmmm20_gan/mp4 -d1 /data/acmmm20_gan/br-400-sal-15
+python compute_face_dists_images.py -A -v -o results_img -d0 /data/acmmm20_gan/mp4 -d1 /data/acmmm20_gan/br-500-sal-15
+python compute_face_dists_images.py -A -v -o results_img -d0 /data/acmmm20_gan/mp4 -d1 /data/acmmm20_gan/br-600-no-saliency
+python compute_face_dists_images.py -A -v -o results_img -d0 /data/acmmm20_gan/mp4 -d1 /data/acmmm20_gan/br-900-sal-15
+python compute_face_dists_images.py -A -v -o results_img -d0 /data/acmmm20_gan/mp4 -d1 /data/acmmm20_gan/br-1000-no-saliency
+python compute_face_dists_images.py -A -v -o results_img -d0 /data/acmmm20_gan/mp4 -d1 /data/acmmm20_gan/br-900-sal-10
+#python compute_face_dists_images.py -A -v -o results_img -d0 /data/acmmm20_gan/mp4 -d1 /data/acmmm20_gan/
