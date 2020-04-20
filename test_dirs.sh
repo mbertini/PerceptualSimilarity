8774a1dbacc91f8a0fa4478fac6d3c16742ff719
Marco Bertini <marco.bertini@unifi.it>
2020-04-20 19:52:45 +0200

    script added

---
+++ test_dirs.sh
@@ -0,0 +1,7 @@
+#python compute_face_dists.py -A -v -d0 ../face_video_masks/orig -d1 ../face_video_masks/no-saliency-br-600
+#python compute_face_dists.py -A -v -d0 ../face_video_masks/orig -d1 ../face_video_masks/saliency-br-400-15
+#python compute_face_dists.py -A -v -d0 ../face_video_masks/orig -d1 ../face_video_masks/saliency-br-500-15
+#python compute_face_dists.py -A -v -d0 ../face_video_masks/orig -d1 ../face_video_masks/saliency-br-300-25
+python compute_face_dists.py -A -v -d0 ../face_video_masks/orig -d1 ../face_video_masks/saliency-br-300-35
+python compute_face_dists.py -A -v -d0 ../face_video_masks/orig -d1 ../face_video_masks/no-saliency-br-1000
+python compute_face_dists.py -A -v -d0 ../face_video_masks/orig -d1 ../face_video_masks/saliency-br-900-15
