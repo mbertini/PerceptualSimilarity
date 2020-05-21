3b489c6ebccf137766c7c24b0264951260a7f201
Marco Bertini <marco.bertini@unifi.it>
2020-05-21 16:46:01 +0200

    comment print debug when skipping images

--- compute_face_dists_images.py
+++ compute_face_dists_images.py
@@ -229,7 +229,7 @@ def main():
         ref_image = cv2.imread(os.path.join(args.dir0, reference_image_name))
         mod_image = cv2.imread(os.path.join(args.dir1, modified_image_name))
         if ref_image is None or mod_image is None:
-            print("Skipping images: {} and {}".format(reference_image_name, modified_image_name))
+            # print("Skipping images: {} and {}".format(reference_image_name, modified_image_name))
             continue
         filename_all = os.path.splitext(reference_image_name)[0] + "-" + os.path.splitext(modified_image_name)[0]
 
